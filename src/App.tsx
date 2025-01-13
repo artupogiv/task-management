3199d1a4a38ef1a5b8bd58629713e15cf01aa03a
Vigo Putra <artupogiv@gmail.com>
2025-01-13 16:42:15 +0700

    feat: create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { TaskList } from "./components/task-list";
+
+const taskItems = [
+  { id: 1, text: "Baca Dokumentasi React", isCompleted: true },
+  { id: 2, text: "Mengerjakan Tugas", isCompleted: true },
+  { id: 2, text: "Belajar Typescript", isCompleted: false },
+];
 export function App() {
   return (
     <div>
-      <h1>Task Management</h1>
+      <header>
+        <h1>Task Management</h1>
+      </header>
+
+      <p className="read-the-docs">Application to manage tasks.</p>
+
+      <TaskList taskItems={taskItems} />
     </div>
   );
 }
