2763f56fb9c48a1ae873342cf934a0be48df08b4
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-31 16:26:24 +0700

    feat: prepare add task form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
+import { useState } from "react";
 import { initialTasks } from "./components/shared/data/initial-tasks";
 import { Footer } from "./components/shared/layout/footer";
 import { Heading } from "./components/shared/layout/header";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import { Button } from "./components/ui/button";
 
 export function App() {
-  const tasks = initialTasks;
+  const [tasks, setTasks] = useState(initialTasks);
 
   return (
     <div className="w-full max-w-5xl mx-auto ">
       <Heading />
 
-      <div className="w-full">
-        <ul>
-          {tasks.map((task) => {
-            return (
-              <li key={task.id}>
-                <h2>{task.title}</h2>
-              </li>
-            );
-          })}
-        </ul>
+      <div className="flex gap-10">
+        <form className="max-w-xs w-full space-y-2">
+          <div>
+            <Label htmlFor="title">Task title</Label>
+            <Input id="title" type="text" placeholder="Makan malam" />
+          </div>
+          <Button type="submit">Add task</Button>
+        </form>
+
+        <div className="w-full">
+          <ul>
+            {tasks.map((task) => {
+              return (
+                <li key={task.id}>
+                  <h2>{task.title}</h2>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
 
       <Footer />
