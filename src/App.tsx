c71935ba1e8f34226e79d7efe13aa5bcbc2d0c88
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-31 16:21:17 +0700

    refactor: simplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { initialTasks } from "./components/shared/data/initial-tasks";
 import { Footer } from "./components/shared/layout/footer";
-import { Heading } from "./components/shared/layout/heading";
-import { Body } from "./components/shared/layout/body";
+import { Heading } from "./components/shared/layout/header";
 
 export function App() {
+  const tasks = initialTasks;
+
   return (
     <div className="w-full max-w-5xl mx-auto ">
       <Heading />
-      <Body />
+
+      <div className="w-full">
+        <ul>
+          {tasks.map((task) => {
+            return (
+              <li key={task.id}>
+                <h2>{task.title}</h2>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
       <Footer />
     </div>
   );
-};
+}
--- src/components/shared/layout/footer.tsx
+++ src/components/shared/layout/footer.tsx
@@ -1,10 +1,19 @@
 export function Footer() {
-    const currentYear = new Date().getFullYear();
-  
-    return (
-      <footer className="w-full text-center py-2 fixed bottom-0 max-w-5xl mx-auto ">
-        <p>&copy; {currentYear} <a href="https://artupogiv.com" target="_blank" className="font-bold text-blue-600">artupogiv </a> • All rights reserved</p>
-      </footer>
-    );
-  }
-  
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="w-full text-center py-2 fixed bottom-0 max-w-5xl mx-auto ">
+      <p>
+        &copy; {currentYear}{" "}
+        <a
+          href="https://artupogiv.com"
+          target="_blank"
+          className="font-bold text-blue-600"
+        >
+          artupogiv{" "}
+        </a>{" "}
+        • All rights reserved
+      </p>
+    </footer>
+  );
+}
