8830234e776fd574e7490e229caeb3c20b462c1d
Vigo Putra <artupogiv@gmail.com>
2025-02-06 11:00:59 +0700

    chore: tidy up

--- src/components/shared/layout/header.tsx
+++ src/components/shared/layout/header.tsx
@@ -5,7 +5,7 @@ export function Heading() {
   return (
     <div className="m-4 p-4 ">
       <header className="flex flex-row justify-between">
-        <h1 className="text-4xl font-medium text-black text-left pb-4 ">
+        <h1 className="text-4xl font-bold text-blue-600 text-left pb-4 ">
           Task Management
         </h1>
         <div className="inline-flex gap-10 items-center">
@@ -21,7 +21,7 @@ export function Heading() {
           </nav>
           <Avatar>
             <AvatarImage src="https://github.com/artupogiv.png" alt="avatar" />
-            <AvatarFallback>FU</AvatarFallback>
+            <AvatarFallback>VP</AvatarFallback>
           </Avatar>
         </div>
       </header>
